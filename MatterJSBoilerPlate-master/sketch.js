97a82d998fb7accdd6ad6a35081042cd4fb15221
pranshi08 <75973520+pranshi08@users.noreply.github.com>
2021-01-02 14:39:27 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+
+	Engine.run(engine);
+	
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  
+  drawSprites();
+ 
+}
+function option(){
+var options ={
+	isstatic : false
+	restitution : 0.3;
+	frictionn : 0.5;
+	density : 1.2;
+}
+}
+
+
+
